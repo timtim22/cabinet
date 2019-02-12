ff6e6762b84db7ed8c2278ada05f6a6455a39601
Taimoor Hassan <tempetationx@hotmail.com>
2019-02-12 12:13:32 +0500

    associtaion bw user and files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -10,11 +10,11 @@ before_action :find_doc, only: [:show, :edit, :update, :destroy]
     end
 
     def new
-        @doc = Doc.new
+        @doc = current_user.docs.build
     end
 
     def create
-        @doc = Doc.new(doc_params)
+        @doc = current_user.docs.build(doc_params)
 
         if @doc.save
             redirect_to @doc
@@ -28,11 +28,16 @@ before_action :find_doc, only: [:show, :edit, :update, :destroy]
     end
 
     def update
-
+        if @doc.update(doc_params)
+            redirect_to @doc
+        else
+            render 'edit'
+        end
     end
 
     def destroy
-
+        @doc.destroy
+        redirect_to docs_path
     end
 
     private
